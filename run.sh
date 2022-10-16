1f7496fa18824949e4ba552ca64cd103384197a5
aaronysj <>
2022-10-16 21:35:48 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 go clean
 PID=$(ps -ef |grep Jarvis | grep -v grep |awk '{print $2}')
 if [ "${PID}" != "" ]; then
-    kill -9 PID
+    kill -9 ${PID}
     echo "Kill PID ${PID}"
 fi
 go build -o Jarvis *.go
