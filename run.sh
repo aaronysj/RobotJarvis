f471c22e697e02ca479dde39e1097c115cec1847
aaronysj <>
2022-10-16 21:27:02 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PID=$(ps -ef |grep Jarvis | grep -v grep |awk '{print $2}')
+if [ ${PID} != "" ]; then
+    kill -9 PID
+    echo "Kill PID ${PID}"
+fi
+go build -o Jarvis *.go
+nohup ./Jarvis >> output.log &
